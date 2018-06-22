aad8166bfa4ef197e0d6c4d902f63897829c8427
kyhyeonswm4.park <kyhyeonswm4.park@lgepartner.com>
2018-06-22 17:54:52 +0900

    Drawing found line is done.

--- line.py
+++ line.py
@@ -57,10 +57,28 @@ class Line:
                 x_idx = np.where((((xval - 25) < x) & (x < (xval + 25)) & ((y < j) & (y < i))))
                 x_window, y_window = x[x_idx], y[x_idx]
                 if np.sum(x_window) != 0:
-                    xvals.append(xvals, x_window)
-                    yvals.append(yvals, y_window)
+                    np.append(xvals, x_window)
+                    np.append(yvals, y_window)
                 i -= 90
                 j -= 90
-        if np.sum(xvals) > 0:
+        if np.sum(xvals) == 0:
             self.found = False
         return xvals, yvals, self.found
+
+    def radius_of_curvature(self, xvals, yvals):
+        ym_per_pix = 30./720
+        xm_per_pix = 3.7/700
+        fit_cr = np.polyfit(yvals*ym_per_pix, xvals*xm_per_pix, 2)
+        curverad = ((1 + (2*fit_cr[0]*np.max(yvals) + fit_cr[1])**2)**1.5) / mp.absolute(2*fit_cr[0])
+        return curverad
+
+    def sort_vals(self, xvals, yvals):
+        sorted_index = np.argsort(yvals)
+        sorted_yvals = yvals[sorted_index]
+        sorted_xvals = xvals[sorted_index]
+        return sorted_xvals, sorted_yvals
+
+    def get_intercepts(self, polynomial):
+        bottom = polynomial[0]*720**2 + polynomial[1]*720 + polynomial[2]
+        top = polynomial[0]*0**2 + polynomial[1]*0 + polynomial[2]
+        return bottom, top
--- main.py
+++ main.py
@@ -39,25 +39,88 @@ def binary_image(img, binary):
 
     return thresholds 
 
-cap = cv2.VideoCapture('/home/kihyeon/Study/python/lane_finding/project_video.mp4')
+def extract_points(xi, yi, line):
+    if line.found == True:
+        sidex, sidey, line.found = line.found_search(xi, yi)
+    if line.found == False:
+        sidex, sidey, line.found = line.blind_search(xi, yi, binary)
+
+    sidey = np.array(sidey).astype(np.float32)
+    sidex = np.array(sidex).astype(np.float32)
+    
+    side_fit = np.polyfit(sidey, sidex, 2)
+    sidex_int, side_top = line.get_intercepts(side_fit)
+
+    line.x_int.append(sidex_int)
+    line.top.append(side_top)
+    sidex_int = np.mean(line.x_int)
+    side_top = np.mean(line.top)
+    line.lastx_int = sidex_int
+    line.last_top  = side_top
+
+    sidex = np.append(sidex, sidex_int)
+    sidey = np.append(sidey, 720)
+    sidex = np.append(sidex, side_top)
+    sidey = np.append(sidey, 0)
+
+    sidex, sidey = line.sort_vals(sidex, sidey)
+    line.X = sidex
+    line.Y = sidey
+
+    side_fit = np.polyfit(sidey, sidex, 2)
+    line.fit0.append(side_fit[0])
+    line.fit1.append(side_fit[1])
+    line.fit2.append(side_fit[2])
+    side_fit = [np.mean(line.fit0),
+                np.mean(line.fit1),
+                np.mean(line.fit2)]
+
+    side_fitx = side_fit[0]*sidey**2 + side_fit[1]*sidey + side_fit[2]
+    line.fitx = side_fitx
+
+    if line.pos == 'left':
+        pts = np.array([np.flipud(np.transpose(np.vstack([line.fitx, line.Y])))])
+    else:
+        pts = np.array([np.transpose(np.vstack([line.fitx, line.Y]))])
+
+    return pts
+
 
 src = np.float32([[ 490,  482], [ 810,  482], [1250,  720], [  40,  720]])
 dst = np.float32([[   0,    0], [1280,    0], [1250,  720], [  40,  720]])
-M = cv2.getPerspectiveTransform(src, dst)
 
+M    = cv2.getPerspectiveTransform(src, dst)
+Minv = cv2.getPerspectiveTransform(dst, src)
+
+Left  = Line('left')
+Right = Line('right')
+
+cap = cv2.VideoCapture('/home/kihyeon/Study/python/lane_finding/project_video.mp4')
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret is False:
         print('read fail')
         break
-   #hls = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)
+
     warped = birds_eye(frame, M)
     binary = apply_thresholds(warped)
-    thresholds = binary_image(warped, binary)
+    #thresholds = binary_image(warped, binary)
+
     x, y = np.nonzero(np.transpose(binary))
-    Left = Line('left')
-    leftx, lefty, Left.found = Left.blind_search(x, y, binary)
-    cv2.imshow('frame', thresholds)
+    left_pts   = extract_points(x, y, Left)
+    right_pts  = extract_points(x, y, Right)
+
+    warp_zero = np.zeros_like(binary).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+    pts = np.hstack((left_pts, right_pts))
+
+    cv2.polylines(color_warp, np.int_([pts]), isClosed=False, color=(255,0,0), thickness=20)
+    cv2.fillPoly(color_warp, np.int_(pts), (34, 255, 34))
+    newwarp = cv2.warpPerspective(color_warp, Minv, (frame.shape[1], frame.shape[0]))
+    result = cv2.addWeighted(frame, 1, newwarp, 0.5, 0)
+
+    cv2.imshow('frame', result)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
